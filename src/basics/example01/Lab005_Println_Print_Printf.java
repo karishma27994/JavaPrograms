fd821601a5a3696b7c31c9d9adb26fc748d8845f
karishma27994 <joshikarishma2409@gmail.com>
2025-09-18 10:13:48 +0530

    Amend commit-18/09/2025 10:13 AM

--- src/basics/example01/Lab005_Println_Print_Printf.java
+++ src/basics/example01/Lab005_Println_Print_Printf.java
@@ -1,13 +1,49 @@
 package basics.example01;
 
+import java.sql.SQLOutput;
+
 public class Lab005_Println_Print_Printf {
     public static void main(String[] args) {
-        System.out.println("Hello123"); //Prints output and move to next line in console
-        System.out.print("Hi"); //Prints output and stays on same line in console
-        System.out.print("Bye");
-        //If we need space between Hi and Bye we can give extra space after "Hi" in print statement
-        System.out.print("Hi ");
-        System.out.print("Bye");
+       //println: Prints output and move to next line in console
+        System.out.println("Hello123");
+
+        //print: Prints output and stays on same line in console
+        System.out.print("Hi");
+        System.out.print("Bye\n");
+
+        System.out.print("Hi\t"); //If we need space between Hi and Bye we can give extra space after "Hi" in print statement
+        System.out.print("Bye\n");
+
+
+        //printf: Printf means print formatted. Using specifiers(placeholders starting with %) we can print output in a nice formatted way.
+       //to print integer numbers we use %d. Example as below:
+        byte a = 1; short b = 2; int i =3; long l = 4L;
+        System.out.printf("Printing a whole no which is of byte type: %d %n" , a);
+        System.out.printf("Printing a whole no which is of short type: %d %n" , b);
+        System.out.printf("Printing a whole no which is of int type: %d%n" , i);
+        System.out.printf("Printing a whole no which is of long type: %d%n" , l);
+
+        //to print floating numbers we use %f. Example as below:
+        float f = 1.2222F; double d=3.144444444;
+        System.out.printf("Printing a decimal no which is of float type: %f%n" , f);
+        System.out.printf("Printing a decimal no which is of double type: %f%n" , d);
+
+        //to print Strings we use %s. Example as below:
+        String str = "Welcome to Java Programming";
+        System.out.printf("Hello %s%n", str);
+
+        //to print single character we use %c. Example as below:
+        char c = 'o';
+        System.out.printf("Hell%c%n", c);
+
+        //to print boolean value we use %b. Example as below:
+        System.out.printf("Is coding fun? %b%n", true);
+
+        //to print % itself we use %%. Example as below:
+        System.out.printf("Battery charged 90%%");
+
+        //For new line we use %n which is used in all the examples shown above
+
 
     }
 }
---
+++ src/basics/example01/Lab007_Primitive_Data_Types.java
@@ -0,0 +1,27 @@
+package basics.example01;
+
+public class Lab007_Primitive_Data_Types {
+    public static void main(String[] args) {
+        byte b1 = -128, b2=127;
+        System.out.println("byte size:" + b1 +"\t"+ b2);
+        short s1 = -32768, s2 = 32767;
+        System.out.println("short size:" + s1 +"\t"+ s2);
+        int i = 1234567890;
+        /*simple terms if number has more than 10 digits, it probably won't fit in int so we need to use long instead.
+        Please note this comment is just for an understanding. In real Java doesn't count digits - it checks if the actual value fits within the range.*/
+        System.out.println(i);
+        long l = 1234567891234567891L;
+        /*simple terms if number has more than 19 digits, it probably won't fit in long.
+        Please note this comment is just for an understanding. In real Java doesn't count digits - it checks if the actual value fits within the range.*/
+        System.out.println(l);
+        char c1='A', c2 ='a', c3=':', c4='水'; //Unicode characters 0 to 65,535
+        System.out.println("Character stores letters&symbols however internally it stores it as Unicode numbers:" +c1 +'\t'+c2+'\t'+c3+'\t'+c4);
+        float f = 311.12345678912345678912345678912345678912345678911111111111111111111111111111111111111111111111111111111111111111111f;
+        System.out.println("Float stores approx 6-7 significant digits of precision:"+f);
+        double d = 311.12345678912345678912345678912345678912345678911111111111111111111111111111111111111111111111111111111111111111111;
+        System.out.println("Double stores approx 15-17 significant digits of precision:"+d);
+        boolean bol1 = true, bol2 = false;
+        System.out.println("Boolean stores only 2 values either true or false:"+bol1+'\t'+bol2);
+
+    }
+}
